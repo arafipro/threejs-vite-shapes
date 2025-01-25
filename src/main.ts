8bc6fbfd30d9ea716e8ffcd56663fa88c15cb5b0
Hiroki Chiyoda <contact@arafipro.com>
2025-01-25 11:05:57 +0900

    04_geo-args

--- src/main.ts
+++ src/main.ts
@@ -23,3 +23,30 @@ const light = new THREE.DirectionalLight(lightColor, intensity);
 light.position.set(-1, 2, 4);
 scene.add(light);
 
+const geometries = [
+  new THREE.SphereGeometry(0.8, 32, 32),
+  new THREE.ConeGeometry(0.8, 1.5, 32),
+  new THREE.TorusGeometry(0.6, 0.2, 16, 100),
+  new THREE.OctahedronGeometry(0.8),
+  new THREE.TetrahedronGeometry(0.8),
+];
+
+const basicMaterial = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+
+geometries.forEach((geo, i) => {
+  const meshBasic = new THREE.Mesh(geo, basicMaterial);
+  meshBasic.position.x = (i - 2) * 2.2;
+	scene.add(meshBasic);
+	
+	function animate() {
+		requestAnimationFrame(animate)
+		
+		meshBasic.rotation.x += 0.01
+		meshBasic.rotation.y += 0.01
+		
+		renderer.render(scene, camera);
+	}
+	
+	animate();
+});
+
